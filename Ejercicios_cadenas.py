15396564f31134e79f79919ba4b67edeb7abc4a3
Cesar Ticora <c.ticora4@gmail.com>
2025-10-01 00:19:50 -0500

    se agregan cambios

---
+++ Ejercicios_cadenas.py
@@ -0,0 +1,43 @@
+#Escribir un pregunte al usuario el nombre del usuario en la conosla y un número entero e imprima
+#por pantalla en línea distitnat el nombre del usuario tantas veces como el número introducido
+# nombre = input("Ingrese su nombre: ")
+# vecesImprimir = int(input("Ingrese el número de veces a imprimir: "))
+
+# for  i in range(vecesImprimir):
+#     print(nombre)
+
+#Solicitar el nombre completo de un usuario y mostralo en mayusculas, miusculas y la primera letra de todo el nombre en mayuscula
+# nombre1 = input("Ingrese su nombre completo: ")
+# print(nombre1.upper())
+# print(nombre1.lower())
+# print(nombre1.title())
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////##
+#Escribir un programa que pregunte el nombre del ususario en la conosla y después de que el usuario lo introduzcamuestre en pantalla
+#nombre tiene n letras, donde nombre es el ususuario en mayusucla y n es el n´´umero de letras que tiene el nombre.
+# contador = 0
+# nombre2 = input("Ingrese el nombre del usuario: \n")
+# for i in nombre2:
+#     contador += 1
+
+# print(contador)
+
+#otra opcion 
+#nombre2 = input("¿Cómo te llamas?")
+#print(nombre2.upper()+"tiene"+str(len(nombre2))+"letras")
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#Los teléfonos de una empresa tiene el siguiente formatio prefijo-numero-extension don el prefijo en el código del país, y la extension tiene dos dígitos
+#(por ejemplo  +34-913724710-56). Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
+
+# telefono = input("Ingrese el numero de telefono")
+# telefonoPrefijo = telefono.split("-")
+# resultado = telefonoPrefijo[1]
+# print(resultado)
+
+
+# tel = input("Introduce un número de teléfono con el formato +xx-xxxxxxxxx-xx: ")
+# print('El número de teléfono es ', tel[4:-3])
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
